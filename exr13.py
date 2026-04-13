fb4725f7478b143d87b1394747c79fb7806cfb9c
lucasgabriellimagaldino-png <lucasgabriellimagaldino@gmail.com>
2026-04-12 23:44:11 -0300

    Refactor number comparison logic in exr13.py

--- exr13.py
+++ exr13.py
@@ -1,5 +1,7 @@
 n1 = int(input("Digite um número: "))
-if n1 > 100/2:
-    print("Se o número é maior que a metade de 100, o resultado é", n1/2)
-elif n1 < 100*2:
-    print("Se o número é menor que 100, o resultado é", n1*2)
+if n1 > 100:
+    resultado = n1 / 2
+    print("Se o número é maior que 100, o resultado é", resultado)
+else:
+    resultado = n1 * 2
+    print("Se o número é menor ou igual a 100, o dobro é", resultado)
